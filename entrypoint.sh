8a954e84b884c56b095450356beff4ca1297d006
Sebastian Gumprich <sebastian.gumprich@t-systems.com>
2022-08-19 12:48:02 +0200

    rename github token

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 
 ARG_TO_VERSION=""
 if [ ! -z "$INPUT_TO_VERSION" ]; then
-  ARG_TO_VERSION="--to_version $TO_VERSION"
+  ARG_TO_VERSION="--to_version $INPUT_TO_VERSION"
 fi
 
 ARG_SINCE_VERSION=""
 if [ ! -z "$INPUT_SINCE_VERSION" ]; then
-  ARG_SINCE_VERSION="--since_version $SINCE_VERSION"
+  ARG_SINCE_VERSION="--since_version $INPUT_SINCE_VERSION"
 fi
 
-echo "antsichaut --github_token $INPUT_GIT_TOKEN $ARG_SINCE_VERSION $ARG_TO_VERSION"
-antsichaut --github_token $INPUT_GIT_TOKEN $ARG_SINCE_VERSION $ARG_TO_VERSION
+echo "antsichaut --github_token $INPUT_GITHUB_TOKEN $ARG_SINCE_VERSION $ARG_TO_VERSION"
+antsichaut --github_token $INPUT_GITHUB_TOKEN $ARG_SINCE_VERSION $ARG_TO_VERSION
