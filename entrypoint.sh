08c6478134abbd70e955d81ccf1bf068deebeff2
Sebastian Gumprich <sebastian.gumprich@t-systems.com>
2022-08-19 11:27:04 +0200

    fix wrong args

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ fi
 
 ARG_SINCE_VERSION=""
 if [ ! -z "$SINCE_VERSION" ]; then
-  ARG_SINCE_VERSION="--to_version $SINCE_VERSION"
+  ARG_SINCE_VERSION="--since_version $SINCE_VERSION"
 fi
 
-antsichaut --token $GITHUB_TOKEN $ARG_SINCE_VERSION $ARG_TO_VERSION
+antsichaut --github_token $GITHUB_TOKEN $ARG_SINCE_VERSION $ARG_TO_VERSION
